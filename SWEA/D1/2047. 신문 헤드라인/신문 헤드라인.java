4fe4322e95f9708983d037969c201ed37845560d
이서현 <77585088+seohyun09@users.noreply.github.com>
2025-12-31 18:47:44 +0900

    [D1] Title: 신문 헤드라인, Time: 76 ms, Memory: 25,088 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine().toUpperCase();
+        System.out.print(s);
+    }
+}
