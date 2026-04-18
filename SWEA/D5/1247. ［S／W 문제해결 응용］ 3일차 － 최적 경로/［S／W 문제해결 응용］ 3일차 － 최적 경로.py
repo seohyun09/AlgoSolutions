352b1ef4ff02e2d627c76037cc28aec1b7ac4a94
이서현 <77585088+seohyun09@users.noreply.github.com>
2026-04-18 19:37:36 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 3,930 ms, Memory: 91,904 KB -BaekjoonHub

---
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
@@ -0,0 +1,35 @@
+t = int(input())
+
+for tc in range(t):
+    n = int(input())
+    sx, sy, tx, ty, *arr = map(int, input().split())
+    visited = [False] * n
+
+    n_arr = []
+    for i in range(0, len(arr), 2):
+        n_arr.append((arr[i], arr[i + 1]))
+
+    answer = 10000
+
+    def recur(prev_x, prev_y, cnt, dist, visited):
+        global answer
+
+        if cnt == n:
+            end_distance = abs(prev_x - tx) + abs(prev_y - ty)
+            answer = min(answer, dist + end_distance)
+            return
+
+        if dist > answer:
+            return
+
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                distance = abs(prev_x - n_arr[i][0]) + abs(prev_y - n_arr[i][1])
+                recur(n_arr[i][0], n_arr[i][1], cnt + 1, dist + distance, visited)
+                visited[i] = False
+
+    for i in range(n):
+        recur(sx, sy, 0, 0, [False] * n)
+
+    print(f"#{tc + 1} {answer}")
