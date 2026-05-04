df7546c3dc2e51660acae49f4c00ef292cc32060
이서현 <77585088+seohyun09@users.noreply.github.com>
2026-05-04 21:49:23 +0900

    [level 2] Title: 큰 수 만들기, Time: 4747.58 ms, Memory: 390 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String number, int k) {
+        
+        return dfs(number, k);
+    }
+    
+    private String dfs(String number, int k) {
+        // 종료조건
+        if (k == 0) {
+            return number;
+        }
+        
+        if (number.length() <= k) return "";
+        
+        int max_val = number.charAt(0) - '0';
+        int max_idx = 0;
+        
+        // k 범위 내 최대값, 최대값 인덱스 저장
+        for (int i = 1; i <= k; i++) {
+            int digit = number.charAt(i) - '0';
+            if (digit > max_val) {
+                max_val = digit;
+                max_idx = i;
+            }
+        }
+
+        return number.charAt(max_idx) + dfs(number.substring(max_idx + 1), k - max_idx);
+    }
+}
