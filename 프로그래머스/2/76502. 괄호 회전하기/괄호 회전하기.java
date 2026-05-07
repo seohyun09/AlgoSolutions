8f7300807252ab9b84c87f29f12a278325d13bd5
이서현 <77585088+seohyun09@users.noreply.github.com>
2026-05-08 00:26:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 31.74 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    static Map<Character, Integer> map = Map.of(
+        '[', 1,
+        ']', 1,
+        '(', 2,
+        ')', 2,
+        '{', 3,
+        '}', 3
+    );
+    
+    public int solution(String s) {
+        int answer = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            char front = s.charAt(0);
+            s = s.substring(1) + front;            
+            
+            if (validate(s)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+        
+    }
+    private boolean validate(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        
+        int idx = 1;
+        
+        while (idx < s.length()) {
+            char op = s.charAt(idx);
+            
+            if (op == '(' || op == '[' || op == '{') {
+                stack.push(op);
+            } else {
+                if (stack.isEmpty()) return false;
+                
+                char pop_op = stack.pop();
+                
+                if (map.get(pop_op) != map.get(op)) return false;
+                
+            }
+            
+            idx++;
+        }
+        
+        if (!stack.isEmpty()) return false;
+        return true;
+    }
+}
