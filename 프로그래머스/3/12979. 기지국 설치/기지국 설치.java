61208c969e6726f2aaea4f4f830d29201c0ad757
이서현 <77585088+seohyun09@users.noreply.github.com>
2026-07-14 14:14:26 +0900

    [level 3] Title: 기지국 설치, Time: 1.05 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int prev = 1;
+        int unit = w * 2 + 1;
+        
+        for (int station : stations) {
+            int start = station - w <= 0 ? 1 : station - w;
+            int end = station + w >= n ? n : station + w;
+            
+            if (start - prev <= 0) {
+                prev = end + 1;
+            } else {
+                int gap = start - prev;                
+                answer += (gap + unit -1) / unit;
+                prev = end + 1;
+            }
+        }
+        
+        if (prev <= n + 1) {
+            int gap = n - prev + 1;
+            answer += (gap + unit - 1) / unit;
+        }
+
+        return answer;
+    }
+}
